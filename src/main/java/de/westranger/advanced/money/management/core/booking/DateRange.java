68bbc1845fcb0d7dfe420ece0d01bc09ef66140b
westranger <marius.hoffmann@westranger.de>
2020-06-28 16:19:28 +0200

    added class for date ranges
    date range test changes
    
    
    Date Range changes
    
    
    refactor Date Range
    
    
    date range changes

---
+++ src/main/java/de/westranger/advanced/money/management/core/booking/DateRange.java
@@ -0,0 +1,37 @@
+package de.westranger.advanced.money.management.core.booking;
+
+import java.util.Date;
+
+public final class DateRange {
+    private final long startDate;
+    private final long endDate;
+
+    public DateRange(final long startDate, final long endDate) {
+        if (startDate > endDate) {
+            throw new IllegalArgumentException("startDate > endDate");
+        }
+
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public long getStartDate() {
+        return startDate;
+    }
+
+    public long getEndDate() {
+        return endDate;
+    }
+
+    public boolean isBetween(final long date) {
+        if (date >= this.startDate && date <= this.endDate) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "DateRange(from:" + (new Date(this.startDate)) + " to:" + (new Date(this.startDate)) + ")";
+    }
+}
---
+++ src/test/java/test/de/westranger/advanced/money/management/core/booking/DateRangeTest.java
@@ -0,0 +1,48 @@
+package test.de.westranger.advanced.money.management.core.booking;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import de.westranger.advanced.money.management.core.booking.DateRange;
+import de.westranger.advanced.money.management.core.util.DateUtil;
+import org.junit.Test;
+
+public class DateRangeTest {
+
+    @Test
+    public void testGetter() {
+        final Date dateStart = DateUtil.createDate(1, Calendar.JANUARY, 2020);
+        final Date dateEnd = DateUtil.createDate(31, Calendar.JANUARY, 2020);
+        final DateRange dr = new DateRange(dateStart.getTime(), dateEnd.getTime());
+
+        assertEquals(dateStart.getTime(), dr.getStartDate());
+        assertEquals(dateEnd.getTime(), dr.getEndDate());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStartDateLargerEndeDate() {
+        final Date dateStart = DateUtil.createDate(1, Calendar.JANUARY, 2020);
+        final Date dateEnd = DateUtil.createDate(31, Calendar.JANUARY, 2020);
+        final DateRange dr = new DateRange(dateEnd.getTime(), dateStart.getTime());
+    }
+
+    @Test
+    public void testBetween() {
+        final Date dateStart = DateUtil.createDate(1, Calendar.JANUARY, 2020);
+        final Date dateEnd = DateUtil.createDate(31, Calendar.JANUARY, 2020);
+        final DateRange dr = new DateRange(dateStart.getTime(), dateEnd.getTime());
+
+        Date dateTooEarly = DateUtil.createDate(1, Calendar.JANUARY, 2019);
+        Date dateBetween = DateUtil.createDate(10, Calendar.JANUARY, 2020);
+        Date dateTooLate = DateUtil.createDate(1, Calendar.JANUARY, 2021);
+
+        assertFalse(dr.isBetween(dateTooEarly.getTime()));
+        assertTrue(dr.isBetween(dateBetween.getTime()));
+        assertFalse(dr.isBetween(dateTooLate.getTime()));
+    }
+}
+
+
+
