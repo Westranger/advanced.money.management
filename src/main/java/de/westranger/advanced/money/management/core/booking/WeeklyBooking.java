af57323e2f6716a0378ce2205eb840b79ab4cd1a
westranger <marius.hoffmann@westranger.de>
2020-07-05 19:54:37 +0200

    Added base class for weekly bookings refactor weekly booking test
    refactor weekly booking

--- src/main/java/de/westranger/advanced/money/management/core/booking/WeeklyBooking.java
+++ src/main/java/de/westranger/advanced/money/management/core/booking/WeeklyBooking.java
@@ -2,26 +2,16 @@ package de.westranger.advanced.money.management.core.booking;
 
 public final class WeeklyBooking extends Booking {
 
-    private final int repetition;
     private final int dayOfWeek;
 
-    public WeeklyBooking(final int repetition, final int dayOfWeek, final double value, final BookingType type, final DateRange range, final String description) {
-        super(value, type, range, description);
-
-        if (repetition < 1) {
-            throw new IllegalArgumentException("repetition out of range: 1 <= " + repetition);
-        }
+    public WeeklyBooking(final int repetition, final int dayOfWeek, final double value, final BookingType type, final DateRange range, final String description, final DateExclusion dateExclusion) {
+        super(repetition, value, type, range, description, dateExclusion);
 
         if (dayOfWeek < 1 || dayOfWeek > 7) {
             throw new IllegalArgumentException("day of week is out of range 1 <= x <= 7 but was " + dayOfWeek);
         }
 
-        this.repetition = repetition;
         this.dayOfWeek = dayOfWeek;
-    }
-
-    public int getRepetition() {
-        return repetition;
     }
 
     public int getDayOfWeek() {
---
+++ src/main/java/de/westranger/advanced/money/management/core/booking/enums/DayOfWeek.java
@@ -0,0 +1,5 @@
+package de.westranger.advanced.money.management.core.booking.enums;
+
+public enum DayOfWeek {
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday, Weekday, Workday;
+}
